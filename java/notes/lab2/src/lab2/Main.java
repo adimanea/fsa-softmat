be15d6d51a581712db0e26313afab5159c223b00
cristinamarghescu <cmarghescu@stud.fsa.upb.ro>
2021-04-05 20:12:46 +0300

    Lab 2

---
+++ java/notes/lab2/src/lab2/Main.java
@@ -0,0 +1,27 @@
+package lab2;
+
+public class Main {
+	static byte aByte;
+	static short aShort;
+	static int anInt;
+	static long aLong;
+	static float aFloat;
+	static double aDouble;
+	static char aChar;
+	static String astring;
+	static boolean aBoolean;
+	
+	public static void main(String[] args) {
+		System.out.println("Default value of a byte is " + aByte);
+		System.out.println("Default value of a short is " + aShort);
+		System.out.println("Default value of an int is " + anInt);
+		System.out.println("Default value of a long is " + aLong);
+		System.out.println("Default value of a floate is " + aFloat);
+		System.out.println("Default value of a double is " + aDouble);
+		System.out.println("Default value of a char is " + aChar);
+		System.out.println("Default value of a string is " + astring);
+		System.out.println("Default value of a boolean is " + aBoolean);
+	}
+
+
+}
